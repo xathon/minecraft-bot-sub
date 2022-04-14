a0a0d05c394dec6bbd641351d472bf92fe1ae5cb
Alex Fuchs <github@xathon.de>
2022-04-14 01:28:01 +0000

    execute start/stop commands only when needed

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 function validate (req) {
-    return process.env.WORLDS.split(',').includes(req.params.worldName) && process.env.SECRET === req.headers.authorization;
+    return JSON.parse(process.env.WORLDS).includes(req.params.worldName) && process.env.SECRET === req.headers.authorization;
 }
 
 app.post('/', (req, res) => {
@@ -20,6 +18,8 @@ app.post('/', (req, res) => {
         res.status(401).send("Unauthorized");
     }
 });
+
+app.get('/status/', (req, res) => { res.status(400).send("Missing worldName") });
 
 app.get('/status/:worldName', (req, res) => {
     const worldName = req.params.worldName;
@@ -37,10 +37,13 @@ app.get('/status/:worldName', (req, res) => {
         res.status(401).send("Unauthorized");
     }
 });
+
+app.get('/logasfile/', (req, res) => { res.status(400).send("Missing worldName") });
+app.get('/logasfile/:worldName', (req, res) => { res.status(400).send("Missing file") });
+
 app.get('/logasfile/:worldName/:file', (req, res) => {
     const worldName = req.params.worldName;
     const file = req.params.file;
-    const pass = req.headers.authorization;
     if(!worldName) res.status(400).send("Missing worldName");
     if(!file) res.status(400).send("Missing file");   
     
@@ -59,72 +62,115 @@ app.get('/logasfile/:worldName/:file', (req, res) => {
 
             res.status(200).download(`${process.env.MSCS_WORLDS}/${worldName}/${file}`);
 
+        } else {
+            res.status(400).send("Invalid file");
         }
 
     }
 
 });
+app.post('/start/', (req, res) => { res.status(400).send("Missing worldName") });
 app.post('/start/:worldName', (req, res) => {
     const worldName = req.params.worldName;
-    const pass = req.headers.authorization;
     if(!worldName) res.status(400).send("Missing worldName");
     if (validate(req)) {
         
 
         require('child_process').exec(`${process.env.MSCS_BIN} status ${worldName}`, (err,stdout,stderr) => {
-            if(stdout.includes("starting up") ) {
-                res.status(425).send("Server is still starting up");
-            }
-        });
-
-        require('child_process').exec(`${process.env.MSCS_BIN} start ${worldName}`, (err,stdout,stderr) => {
             if (err) {
                 console.log(err);
                 res.status(500).send(err);
-            } else {
-                res.status(200).send(stdout);
             }
+            if(stdout.includes("starting up") ) {
+                res.status(425).send("Server is still starting up");
+            } else if(stdout.includes("running version") ) {
+                res.status(200).send("Server is already running");
+            } else {
+                require('child_process').exec(`${process.env.MSCS_BIN} start ${worldName}`, (err,stdout,stderr) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                } else {
+                    res.status(202).send(stdout);
+                }
+            });
+            }
+            
+            
         });
+
+        
     } else {
         res.status(401).send("Unauthorized");
     }
 });
+app.post('/stop/', (req, res) => { res.status(400).send("Missing worldName") });
 app.post('/stop/:worldName', (req, res) => {
     const worldName = req.params.worldName;
-    const pass = req.headers.authorization;
     if(!worldName) res.status(400).send("Missing worldName");
 
     if (validate(req)) {
         
-        require('child_process').exec(`${process.env.MSCS_BIN} stop ${worldName}`, (err,stdout,stderr) => {
+        require('child_process').exec(`${process.env.MSCS_BIN} status ${worldName}`, (err,stdout,stderr) => {
             if (err) {
                 console.log(err);
                 res.status(500).send(err);
-            } else {
-                res.status(200).send(stdout);
             }
+            console.log(stdout);
+            if(stdout.includes("starting up") ) {
+                res.status(425).send("Server is still starting up");
+            }
+            if(stdout.includes("not running") ) {
+                res.status(200).send("Server is already stopped");
+            }
+            require('child_process').exec(`${process.env.MSCS_BIN} stop ${worldName}`, (err,stdout,stderr) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                } else {
+                    res.status(200).send(stdout);
+                }
+            });
         });
     } else {
         res.status(401).send("Unauthorized");
     }
 });
-app.post('/restart/:worldName', (req, res) => {
+app.post('/restart/', (req, res) => { res.status(400).send("Missing worldName") });
+app.post('/restart/:worldName', (req, res) => { // TODO this
     const worldName = req.params.worldName;
-    const pass = req.headers.authorization;
     if(!worldName) res.status(400).send("Missing worldName");
     if (validate(req)) {
-        require('child_process').exec(`${process.env.MSCS_BIN} restart ${worldName}`, (err,stdout,stderr) => {
+        require('child_process').exec(`${process.env.MSCS_BIN} status ${worldName}`, (err,stdout,stderr) => {
             if (err) {
                 console.log(err);
                 res.status(500).send(err);
-            } else {
-                res.status(200).send(stdout);
             }
+            console.log(stdout);
+            if(stdout.includes("starting up") ) {
+                res.status(425).send("Server is still starting up");
+            }
+            require('child_process').exec(`${process.env.MSCS_BIN} restart ${worldName}`, (err,stdout,stderr) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                } else {
+                    res.status(200).send(stdout);
+                }
+            });
         });
     } else {
         res.status(401).send("Unauthorized");
     }
 });
 
+
+app.get('/',function (req, res) {
+    res.redirect('https://www.youtube.com/watch?v=o-YBDTqX_ZU'); // because why not
+});
+
+app.use(function(req, res){
+    res.sendStatus(404);
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
